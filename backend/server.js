ab74e61515c56907ded6a283723db4a64c306c8a
Huy <trandoanhuy.work@gmail.com>
2022-04-10 09:52:33 +0700

    up code project ecommerce

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const app = require('./app')
+
+const dotenv = require('dotenv')
+const connectDatabase = require('./config/database')
+
+// Handling Uncaught Exception
+process.on('uncaughtException', err => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`)
+
+    process.exit(1)
+})
+
+// config
+dotenv.config({ path: 'backend/config/config.env' })
+
+// connecting to database
+connectDatabase()
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server on working on http://loaclhost:${process.env.PORT}`)
+})
+
+// Unhandled Promise Rejection
+process.on('unhandledRejection', err => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandle Promise Rejection`)
+
+    server.close(() => {
+        process.exit(1)
+    })
+})
